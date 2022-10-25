2b48a30d146e2b1a4f35800437f44c8d5a0bdc84
Andy Delso <andy@delso.me>
2022-10-25 11:27:42 -0500

    update Results fragment to use composables

--- app/src/main/java/com/example/guessinggame/ResultFragment.kt
+++ app/src/main/java/com/example/guessinggame/ResultFragment.kt
@@ -5,14 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
-import com.example.guessinggame.databinding.FragmentResultBinding
 
 class ResultFragment : Fragment() {
-
-    private var _binding: FragmentResultBinding? = null
-    private val binding get() = _binding!!
     lateinit var viewModel: ResultViewModel
     lateinit var viewModelFactory: ResultViewModelFactory
 
@@ -20,24 +28,48 @@ class ResultFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment from the view binding
-        _binding = FragmentResultBinding.inflate(inflater, container, false)
-        val view = binding.root
-
         val result = ResultFragmentArgs.fromBundle(requireArguments()).result
 
         viewModelFactory = ResultViewModelFactory(result)
-        viewModel = ViewModelProvider(this, viewModelFactory).get(ResultViewModel::class.java)
+        viewModel = ViewModelProvider(this@ResultFragment, viewModelFactory).get(ResultViewModel::class.java)
 
-        // setup data binding
-
-        binding.resultViewModel = viewModel
-
-        binding.newGameButton.setOnClickListener {
-            val action = ResultFragmentDirections.actionResultFragmentToGameFragment()
-            view.findNavController().navigate(action)
+        return ComposeView(requireContext()).apply {
+            setContent {
+                MaterialTheme {
+                    Surface {
+                        view?.let { ResultFragmentContent(view = it, viewModel = viewModel) }
+                    }
+                }
+            }
         }
+    }
 
-        return view
+    @Composable
+    fun ResultFragmentContent(view: View, viewModel: ResultViewModel) {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            ResultText(result = viewModel.result)
+            NewGameButton {
+                view.findNavController().navigate(R.id.action_resultFragment_to_gameFragment)
+            }
+        }
+    }
+
+    @Composable
+    fun NewGameButton(clicked: () -> Unit) {
+        Button(onClick = clicked) {
+            Text(text = "Start New Game")
+        }
+    }
+
+    @Composable
+    fun ResultText(result: String) {
+        Text(
+            text = result,
+            fontSize = 28.sp,
+            textAlign = TextAlign.Center
+        )
     }
 }
